5ada108f2aed9f415cc1f440169d77b7e27212f6
pfyy <112098515+pfyy@users.noreply.github.com>
2025-08-16 19:17:07 +0800

    fix test

--- test/conftest.py
+++ test/conftest.py
@@ -8,10 +8,13 @@ if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
-@pytest_asyncio.fixture(loop_scope="session")
+@pytest_asyncio.fixture(loop_scope="session", scope="session")
 async def db_pool_fixture():
     if IS_DB_READY:
         pool = get_db_conn_or_pool()
         await pool.open()
 
     yield
+
+    if IS_DB_READY:
+        await pool.close()
