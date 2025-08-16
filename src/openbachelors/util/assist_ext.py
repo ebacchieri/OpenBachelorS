f84470d1b251ab5aec13e7caa58fbbfc3301afb9
pfyy <112098515+pfyy@users.noreply.github.com>
2025-08-16 23:12:55 +0800

    assist ext

---
+++ src/openbachelors/util/assist_ext.py
@@ -0,0 +1,14 @@
+from .const_json_loader import ConstJson
+
+profession_lst = ConstJson(
+    [
+        "PIONEER",
+        "WARRIOR",
+        "TANK",
+        "SNIPER",
+        "CASTER",
+        "MEDIC",
+        "SUPPORT",
+        "SPECIAL",
+    ]
+)
