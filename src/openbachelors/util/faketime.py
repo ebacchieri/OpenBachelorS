3e06b6dd06478c311d554343dfff348283af6dbe
pfyy <112098515+pfyy@users.noreply.github.com>
2025-08-23 14:00:13 +0800

    faketime

--- src/openbachelors/util/faketime.py
+++ src/openbachelors/util/faketime.py
@@ -1,6 +1,9 @@
 import time
 
-FAKETIME = -1
+from .const_json_loader import const_json_loader
+from ..const.filepath import CONFIG_JSON
+
+FAKETIME = const_json_loader[CONFIG_JSON]["faketime"]
 
 
 def faketime():
